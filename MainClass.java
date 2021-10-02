3520878326398fba3cd82e50220e01d06608ef77
Sahilsingggh <91721828+Sahilsingggh@users.noreply.github.com>
2021-10-02 22:57:46 +0530

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -13,6 +13,8 @@ public class MainClass {
     
     public static void main(String[] args) {
         System.out.println("hello world");
+         System.out.println("hello github");
+         System.out.println("hello sahil");
     }
 }
 
